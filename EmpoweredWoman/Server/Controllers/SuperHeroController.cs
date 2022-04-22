9ed8f12c05fce017655abe1d79f5fc3deab09f2b
Maria de Lourdes Celeski <marialceleski@gmail.com>
2022-04-22 15:42:54 -0300

    Adicionar arquivos de projeto.

---
+++ EmpoweredWoman/Server/Controllers/SuperHeroController.cs
@@ -0,0 +1,52 @@
+﻿using EmpoweredWoman.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmpoweredWoman.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        public static List<Position> positions = new List<Position>
+        {
+            new Position{ Id = 1, Name = "Developer"},
+            new Position{ Id = 2, Name = "Teacher" },
+            new Position{ Id = 3, Name = "Assistant" },
+            new Position{ Id = 4, Name = "Administrative" },
+            new Position{ Id = 5, Name = "Principal" }
+        };
+
+        public static List<SuperHero> heroes = new List<SuperHero>
+        {
+            new SuperHero
+            {
+                Id = 1,
+                FirstName = "Maria",
+                LastName = "Celeski",
+                Country = "Brazil",
+                Company = "PrograMaria",
+                FinancialBank = "",
+                Actions = "",
+                LocalActions = "",
+                Position = positions[0]
+            },
+
+            new SuperHero
+            {
+                Id = 2,
+                FirstName = "Jessica",
+                LastName = "Matheus",
+                Country = "Brazil",
+                Company = "PyLadies",
+                FinancialBank = "",
+                Actions = "",
+                LocalActions = "",
+                Position = positions[1]
+
+            },
+        };
+
+    }
+}
+
---
+++ EmpoweredWoman/Shared/SuperHero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpoweredWoman.Shared
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string Company { get; set; } = string.Empty;
+        public string FinancialBank { get; set; } = string.Empty;
+        public string Actions { get; set; } = string.Empty;
+        public string LocalActions { get; set; } = string.Empty;
+        public Position? Position { get; set; }
+        public int PositionId { get; set; }
+    }
+}
